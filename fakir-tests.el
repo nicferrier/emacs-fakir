a35bd523c63267c74c0694fa3c89de13bdd04df5
Nic Ferier <nic@ferrier.me.uk>
2013-02-26 00:34:11 +0000

    remove the tests to a separate file, add insert-file-contents to fakir-mock-process

---
+++ fakir-tests.el
@@ -0,0 +1,196 @@
+;;; Tests for fakir
+
+(require 'fakir)
+
+(ert-deftest flet-overrides ()
+  "Test the flet-override stuff."
+  (flet ((my-test (x)
+                  (and
+                   (listp x)
+                   (let ((v (car x)))
+                     (eq :object v))))
+         (my-func (x y)
+                  (format "strings: %s %s" x y))
+         (my-proc (z)
+                  (* 2 x)))
+    (flet-overrides
+      my-test ; the type predicate we'll use
+      ((my-func a (a b)
+                (+ (cadr a) b))
+       (my-proc y (x y)
+                (+ 10 y)))
+      (should
+       (equal
+        '("strings: nic caroline" 7)
+        (list
+         ;; This call doesn't match the predicate
+         (my-func "nic" "caroline")
+         ;; This call does match the predicate
+         (my-func '(:object 5) 2)))))))
+
+
+(ert-deftest fakir--make-hash-table ()
+  "Test hash table construction."
+  (let ((h (fakir--make-hash-table '((a 10)
+                                     (b 20)
+                                     (fakir-alist-value "is a string")
+                                     fakir-single-value
+                                     :self-evaling-symbol-as-well))))
+    (should (equal 10 (gethash 'a h)))
+    (should (equal 20 (gethash 'b h)))
+    (should (equal nil (gethash 'fakir-single-value h)))
+    (should (equal nil (gethash ':self-evaling-symbol-as-well h)))))
+
+
+(ert-deftest fakir-mock-process ()
+  "Basic test of the mock process."
+  :tags '(unit)
+  (let ((x (fakir-test-mock-process)))
+    (should (equal
+             '(15 30)
+             x))))
+
+(ert-deftest fakir-mock-process-delete ()
+  "Test the delete handling."
+  :tags '(unit)
+  ;; delete-process causes the body to return :mock-process-finished
+  (should
+   (fakir-mock-process
+     :fakeproc
+     ((a 20)
+      (:somevar "somevar"))
+     (let ((x "a string of text"))
+       (delete-process :fakeproc))))
+  ;; How to use catch inside the BODY to handle delete-process
+  (should
+   (equal
+    "the process finished"
+    (fakir-mock-process
+      :fakeproc
+      ((a 20)
+       (:somevar "somevar"))
+      (let ((x "a string of text"))
+	(when (eq :mock-process-finished
+		  (catch :mock-process-finished
+		    (delete-process :fakeproc)))
+	  "the process finished"))))))
+
+
+
+(ert-deftest fakir--file-fqn ()
+  "Test we can make fully qualified names for files."
+  (let ((ef (make-fakir-file
+             :filename "somefile"
+             :directory "/home/dir")))
+    (should (equal "/home/dir/somefile"
+                   (fakir--file-fqn ef)))))
+(ert-deftest fakir--file-mod-time ()
+  "Test that file mtimes are encoded properly."
+  (let ((ef (make-fakir-file
+             :filename "somefile"
+             :directory "/home/dir"
+             :mtime "Mon, Feb 27 2012 22:10:21 GMT")))
+    (should (equal (fakir--file-mod-time ef)
+                   '(20299 65357)))))
+
+(ert-deftest fakir--file-attribs ()
+  "Test that we get back file attributes."
+  (let ((ef (make-fakir-file
+             :filename "somefile"
+             :directory "/home/dir"
+             :mtime "Mon, Feb 27 2012 22:10:21 GMT")))
+    (should (equal
+             (list t t t t t '(20299 65357))
+             (fakir--file-attribs ef)))))
+
+(ert-deftest fakir--file-home ()
+  "Test the home root stuff."
+  (let ((ef (make-fakir-file
+             :filename "somefile"
+             :directory "/home/dir"))
+        (ef2 (make-fakir-file
+             :filename "somefile"
+             :directory "/var/dir"))
+        (ef3 (make-fakir-file
+              :filename "somefile"
+              :directory "/home/dir/someddir")))
+    (should (equal "/home/dir" (fakir--file-home ef)))
+    (should (equal "/home/dir" (fakir--file-home ef3)))
+    (should (equal nil (fakir--file-home ef2)))))
+
+(ert-deftest fakir--expand-file-name ()
+  "Test expanding names to absolutes."
+  (should
+   (equal
+    (fakir--expand-file-name
+     "blah"
+     "/home/emacsuser")
+    "/home/emacsuser/blah"))
+  (should
+   (equal
+    (fakir--expand-file-name
+     "/home/emacsuser/bladh/qdqnwd/qwdqdq.1"
+     "/home/emacsuser")
+    "/home/emacsuser/bladh/qdqnwd/qwdqdq.1"))
+  (should
+   (equal
+    (fakir--expand-file-name
+     "/home/emacsuser/bladh/../qwdqdq.2"
+     "/home/emacsuser")
+    "/home/emacsuser/qwdqdq.2"))
+  (should
+   (equal
+    (fakir--expand-file-name
+     "qwdqdq.3"
+     "/home")
+    "/home/qwdqdq.3"))
+  (should
+   (equal
+    (fakir--expand-file-name
+     "/qwdqdq.4"
+     "/home")
+    "/qwdqdq.4"))
+  (should
+   (equal
+    (fakir--expand-file-name
+     "/home/emacsuser/bladh/../../../../../../qwdqdq.5"
+     "/home")
+    "/qwdqdq.5")))
+
+(ert-deftest fakir--find-file ()
+  (let ((f (fakir-file :filename "README"
+                       :directory "/home/fakir"
+                       :content "This is a ReadMe file.")))
+    (let ((buf (fakir--find-file f)))
+      (unwind-protect
+          (with-current-buffer buf
+            (should
+             (equal "This is a ReadMe file."
+                    (buffer-substring (point-min) (point-max)))))
+        (kill-buffer buf)))))
+
+(ert-deftest fakir-mock-file ()
+  "Test the mock file macro."
+  (fakir-mock-file (make-fakir-file
+                      :filename "somefile"
+                      :directory "/home/test"
+                      :content "This is a file."
+                      :mtime "Mon, Feb 27 2012 22:10:21 GMT")
+    (let ((buf (find-file "/home/test/somefile")))
+      (unwind-protect
+          (with-current-buffer buf
+            (should
+             (equal "This is a file."
+                    (buffer-substring (point-min) (point-max)))))
+        (kill-buffer buf)))
+    (should (file-exists-p "/home/test/somefile"))
+    (should-not (file-exists-p "/home/test/otherfile"))
+    (should-not (file-exists-p "/home/dir/somefile"))
+    (should (equal
+             (expand-file-name "~/somefile")
+             "/home/test/somefile"))
+    (should (equal
+             '(20299 65357)
+             (elt (file-attributes "/home/test/somefile") 5)))))
+
+;;; fakir-tests.el ends here
--- fakir.el
+++ fakir.el
@@ -5,7 +5,7 @@
 ;; Maintainer: Nic Ferrier <nferrier@ferrier.me.uk>
 ;; URL: http://github.com/nicferrier/emacs-fakir
 ;; Created: 17th March 2012
-;; Version: 0.0.15
+;; Version: 0.0.16
 ;; Keywords: lisp, tools
 
 ;; This file is NOT part of GNU Emacs.
@@ -112,32 +112,6 @@ that operate on a particular type, processes for example:
        (flet ,flets
          ,@form))))
 
-(ert-deftest flet-overrides ()
-  "Test the flet-override stuff."
-  (flet ((my-test (x)
-                  (and
-                   (listp x)
-                   (let ((v (car x)))
-                     (eq :object v))))
-         (my-func (x y)
-                  (format "strings: %s %s" x y))
-         (my-proc (z)
-                  (* 2 x)))
-    (flet-overrides
-      my-test ; the type predicate we'll use
-      ((my-func a (a b)
-                (+ (cadr a) b))
-       (my-proc y (x y)
-                (+ 10 y)))
-      (should
-       (equal
-        '("strings: nic caroline" 7)
-        (list
-         ;; This call doesn't match the predicate
-         (my-func "nic" "caroline")
-         ;; This call does match the predicate
-         (my-func '(:object 5) 2)))))))
-
 
 ;; Mocking processes
 
@@ -165,18 +139,6 @@ The ALIST looks like a let-list."
            (t
             (puthash f nil bindings))))
     bindings))
-
-(ert-deftest fakir--make-hash-table ()
-  "Test hash table construction."
-  (let ((h (fakir--make-hash-table '((a 10)
-                                     (b 20)
-                                     (fakir-alist-value "is a string")
-                                     fakir-single-value
-                                     :self-evaling-symbol-as-well))))
-    (should (equal 10 (gethash 'a h)))
-    (should (equal 20 (gethash 'b h)))
-    (should (equal nil (gethash 'fakir-single-value h)))
-    (should (equal nil (gethash ':self-evaling-symbol-as-well h)))))
 
 (defun fakir--get-or-create-buf (pvbuf pvvar &optional specified-buf)
   "Special get or create to support the process mocking.
@@ -322,38 +284,6 @@ In normal circumstances, we return what the BODY returned."
 	  (setq a (process-get :fakeproc :somevar))
 	  (list a (process-get :fakeproc :othervar)))))))
 
-(ert-deftest fakir-mock-process ()
-  "Basic test of the mock process."
-  :tags '(unit)
-  (let ((x (fakir-test-mock-process)))
-    (should (equal
-             '(15 30)
-             x))))
-
-(ert-deftest fakir-mock-process-delete ()
-  "Test the delete handling."
-  :tags '(unit)
-  ;; delete-process causes the body to return :mock-process-finished
-  (should
-   (fakir-mock-process
-     :fakeproc
-     ((a 20)
-      (:somevar "somevar"))
-     (let ((x "a string of text"))
-       (delete-process :fakeproc))))
-  ;; How to use catch inside the BODY to handle delete-process
-  (should
-   (equal
-    "the process finished"
-    (fakir-mock-process
-      :fakeproc
-      ((a 20)
-       (:somevar "somevar"))
-      (let ((x "a string of text"))
-	(when (eq :mock-process-finished
-		  (catch :mock-process-finished
-		    (delete-process :fakeproc)))
-	  "the process finished"))))))
 
 ;; Time utils
 
@@ -401,14 +331,6 @@ was written."
            (fakir-file-filename file))))
     fqfn))
 
-(ert-deftest fakir--file-fqn ()
-  "Test we can make fully qualified names for files."
-  (let ((ef (make-fakir-file
-             :filename "somefile"
-             :directory "/home/dir")))
-    (should (equal "/home/dir/somefile"
-                   (fakir--file-fqn ef)))))
-
 (defun fakir--file-mod-time (file &optional raw)
   "Return the encoded mtime of FILE, an `fakir--file'.
 
@@ -418,15 +340,6 @@ If RAW is t then return the raw value, a string."
       (fakir-file-mtime file)
     (fakir-time-encode (fakir-file-mtime file))))
 
-(ert-deftest fakir--file-mod-time ()
-  "Test that file mtimes are encoded properly."
-  (let ((ef (make-fakir-file
-             :filename "somefile"
-             :directory "/home/dir"
-             :mtime "Mon, Feb 27 2012 22:10:21 GMT")))
-    (should (equal (fakir--file-mod-time ef)
-                   '(20299 65357)))))
-
 (defun fakir--file-attribs (file)
   "Return an answer as `file-attributes' for FILE.
 
@@ -434,16 +347,6 @@ Currently WE ONLY SUPPORT MODIFIED-TIME."
   (fakir--file-check file)
   (list t t t t t
         (fakir--file-mod-time file)))
-
-(ert-deftest fakir--file-attribs ()
-  "Test that we get back file attributes."
-  (let ((ef (make-fakir-file
-             :filename "somefile"
-             :directory "/home/dir"
-             :mtime "Mon, Feb 27 2012 22:10:21 GMT")))
-    (should (equal
-             (list t t t t t '(20299 65357))
-             (fakir--file-attribs ef)))))
 
 (defun fakir--file-home (file)
   "Return the home part of FILE or nil.
@@ -461,21 +364,6 @@ part."
                  fqn)
               (match-string 1 fqn)))))
     home-root))
-
-(ert-deftest fakir--file-home ()
-  "Test the home root stuff."
-  (let ((ef (make-fakir-file
-             :filename "somefile"
-             :directory "/home/dir"))
-        (ef2 (make-fakir-file
-             :filename "somefile"
-             :directory "/var/dir"))
-        (ef3 (make-fakir-file
-              :filename "somefile"
-              :directory "/home/dir/someddir")))
-    (should (equal "/home/dir" (fakir--file-home ef)))
-    (should (equal "/home/dir" (fakir--file-home ef3)))
-    (should (equal nil (fakir--file-home ef2)))))
 
 (defun fakir--file-exists-p (file-name fqfn)
   (equal file-name fqfn))
@@ -505,62 +393,11 @@ part."
      (when (equal ?\/ (elt file-path 0)) "/")
      (mapconcat 'identity (reverse res) "/"))))
 
-(ert-deftest fakir--expand-file-name ()
-  "Test expanding names to absolutes."
-  (should
-   (equal
-    (fakir--expand-file-name
-     "blah"
-     "/home/emacsuser")
-    "/home/emacsuser/blah"))
-  (should
-   (equal
-    (fakir--expand-file-name
-     "/home/emacsuser/bladh/qdqnwd/qwdqdq.1"
-     "/home/emacsuser")
-    "/home/emacsuser/bladh/qdqnwd/qwdqdq.1"))
-  (should
-   (equal
-    (fakir--expand-file-name
-     "/home/emacsuser/bladh/../qwdqdq.2"
-     "/home/emacsuser")
-    "/home/emacsuser/qwdqdq.2"))
-  (should
-   (equal
-    (fakir--expand-file-name
-     "qwdqdq.3"
-     "/home")
-    "/home/qwdqdq.3"))
-  (should
-   (equal
-    (fakir--expand-file-name
-     "/qwdqdq.4"
-     "/home")
-    "/qwdqdq.4"))
-  (should
-   (equal
-    (fakir--expand-file-name
-     "/home/emacsuser/bladh/../../../../../../qwdqdq.5"
-     "/home")
-    "/qwdqdq.5")))
-
 (defun fakir--find-file (fakir-file)
   (let ((buf (get-buffer-create (fakir-file-filename fakir-file))))
     (with-current-buffer buf
       (insert (fakir-file-content fakir-file)))
     buf))
-
-(ert-deftest fakir--find-file ()
-  (let ((f (fakir-file :filename "README"
-                       :directory "/home/fakir"
-                       :content "This is a ReadMe file.")))
-    (let ((buf (fakir--find-file f)))
-      (unwind-protect
-          (with-current-buffer buf
-            (should
-             (equal "This is a ReadMe file."
-                    (buffer-substring (point-min) (point-max)))))
-        (kill-buffer buf)))))
 
 (defmacro fakir-mock-file (fakir-file &rest body)
   "Mock the filesystem with the FAKIR-FILE object.
@@ -581,7 +418,7 @@ The operations that are supported by the fleted functions are:
 `file-attributes', `file-exists-p' and `expand-file-name'. Others
 will be added as necessary."
   (declare (debug (sexp &rest form))
-           (indent defun))
+           (indent 1))
   (let ((fv (make-symbol "fakir-filev")))
     `(let*
          ((,fv ,fakir-file)
@@ -599,6 +436,9 @@ will be added as necessary."
                (fakir--expand-file-name
                 file-name
                 (or def-dir home-root)))
+              (insert-file-contents
+                  (file-name)
+                (insert (fakir-file-content ,fv)))
               (find-file
                (file-name)
                (fakir--find-file ,fv))
@@ -606,30 +446,6 @@ will be added as necessary."
                (file-name)
                (fakir--find-file ,fv)))
          ,@body))))
-
-(ert-deftest fakir-mock-file ()
-  "Test the mock file macro."
-  (fakir-mock-file (make-fakir-file
-                      :filename "somefile"
-                      :directory "/home/test"
-                      :content "This is a file."
-                      :mtime "Mon, Feb 27 2012 22:10:21 GMT")
-    (let ((buf (find-file "/home/test/somefile")))
-      (unwind-protect
-          (with-current-buffer buf
-            (should
-             (equal "This is a file."
-                    (buffer-substring (point-min) (point-max)))))
-        (kill-buffer buf)))
-    (should (file-exists-p "/home/test/somefile"))
-    (should-not (file-exists-p "/home/test/otherfile"))
-    (should-not (file-exists-p "/home/dir/somefile"))
-    (should (equal
-             (expand-file-name "~/somefile")
-             "/home/test/somefile"))
-    (should (equal
-             '(20299 65357)
-             (elt (file-attributes "/home/test/somefile") 5)))))
 
 (provide 'fakir)
 ;
