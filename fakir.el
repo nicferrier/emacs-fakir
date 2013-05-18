f0969d1b26c8e24328ee7f7718fa6744c9b7e580
Nic Ferier <nic@ferrier.me.uk>
2013-05-18 10:31:04 +0100

    bump the version

--- fakir.el
+++ fakir.el
@@ -5,7 +5,7 @@
 ;; Maintainer: Nic Ferrier <nferrier@ferrier.me.uk>
 ;; URL: http://github.com/nicferrier/emacs-fakir
 ;; Created: 17th March 2012
-;; Version: 0.1.0
+;; Version: 0.1.1
 ;; Keywords: lisp, tools
 ;; Package-Requires: ((noflet "0.0.1"))
 
